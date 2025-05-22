ab0cb515bfcbc4066e38f23c1eaede58a0d2b8c5
vipiBitbucket <vipivipisanan@gmail.com>
2025-05-22 21:45:52 +0530

    first commit with DB setup and dependencies

---
+++ src/main/java/com/zerobeta/publish_content/PublishContentApplication.java
@@ -0,0 +1,13 @@
+package com.zerobeta.publish_content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublishContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublishContentApplication.class, args);
+	}
+
+}
